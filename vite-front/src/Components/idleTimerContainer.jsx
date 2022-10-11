1a6fbcf562276da19cb615bd0bd4d901397e95bf
tomermk <tomermk@gmail.com>
2022-10-11 23:54:24 +0300

    added session timer and auto logoff after 20 min

---
+++ vite-front/src/Components/idleTimerContainer.jsx
@@ -0,0 +1,80 @@
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useIdleTimer, workerTimers} from 'react-idle-timer'
+import {Modal} from 'antd';
+
+export const IdleTimerContainer = ({children}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [timer, setTimer] = useState(60);
+    const navigate = useNavigate();
+
+    const onIdle = () => {
+        console.log("User is idle");
+        setIsModalOpen(false);
+        navigate("/login");
+    }
+
+
+    const onActive = () => {
+        console.log('User is active');
+
+    }
+
+
+    const onPrompt = () => {
+        console.log("Prompt is active");
+        setIsModalOpen(true);
+    }
+
+    const idleTimer = useIdleTimer({
+        timeout: 1000 * 60 * 20,
+        promptTimeout: 1000 * 60,
+        timer: workerTimers,
+        onIdle,
+        onActive,
+        onPrompt
+    })
+
+    useEffect( () => {
+        const timeOut = setInterval( () => {
+            setTimer(Math.floor(idleTimer.getRemainingTime()/ 1000));
+        }, 1000);
+        return () => clearInterval(timeOut);
+    },[]);
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+        localStorage.removeItem("auth-jwt");
+        navigate("/login");
+      };
+
+    const handleCancel = () => {+
+        setIsModalOpen(false);
+        idleTimer.activate();
+      };
+
+
+    return (
+        <div>
+            {children}
+            <Modal
+            title="Where did you go?"
+            open={isModalOpen}
+            visible={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            okText="Logout"
+            okButtonProps={{
+                danger: true,
+                type: "primary"
+            }}
+            cancelText="Stay"
+            cancelButtonProps={{
+                type: "primary"
+            }}
+            >
+                <p>You are about to be signed out in {timer} seconds</p>
+            </Modal>
+        </div>
+    )
+}
--- vite-front/src/Dashboard.jsx
+++ vite-front/src/Dashboard.jsx
@@ -5,6 +5,7 @@ import StatisticsPage from './StatisticsPage'
 import NotFound from './NotFound';
 import {Routes,Route,useNavigate, useLocation } from "react-router-dom"
 import { Layout } from 'antd';
+import { IdleTimerContainer } from './Components/idleTimerContainer';
 const {Content, Sider} = Layout
 
 function Dashboard() {
@@ -20,7 +21,7 @@ function Dashboard() {
 
   function handleMenuClick(target){
     if( target.key === 'signout'){
-      localStorage.removeItem('auth-jwt');
+      localStorage.clear();
       navigate("/login", {state: {collapsed: collapsed}});
       setCurrent(target.key);
     } else {
@@ -31,22 +32,24 @@ function Dashboard() {
 
   return (
     <>
-    <Layout style={{ minHeight: '100vh',}}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value => setCollapsed(value))}>
-        <div className='logo'/>
-        <SideMenu current={current} onMenuClick={handleMenuClick}/>
-      </Sider>
-      <Layout className='site-layout'>
-        <Content style={{ margin: '0 16px',}}>
-          <Routes>
-              <Route exact path="/" element={<StatisticsPage/>}/>
-              <Route exact path="/attack1" element={<h1>This is the attack page 1</h1>}/>
-              <Route exact path="/attack2" element={<h1>This is the attack page 2</h1>}/>
-              <Route path='/*' element={<NotFound/>} />
-          </Routes>
-        </Content>
+    <IdleTimerContainer>
+      <Layout style={{ minHeight: '100vh',}}>
+        <Sider collapsible collapsed={collapsed} onCollapse={(value => setCollapsed(value))}>
+          <div className='logo'/>
+          <SideMenu current={current} onMenuClick={handleMenuClick}/>
+        </Sider>
+        <Layout className='site-layout'>
+          <Content style={{ margin: '0 16px',}}>
+            <Routes>
+                <Route exact path="/" element={<StatisticsPage/>}/>
+                <Route exact path="/attack1" element={<h1>This is the attack page 1</h1>}/>
+                <Route exact path="/attack2" element={<h1>This is the attack page 2</h1>}/>
+                <Route path='/*' element={<NotFound/>} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </IdleTimerContainer>
     </>
   )
 }
