e9de75e161f68b53f6a23f7ac0d9abd68e6987d0
Cyber Team <cyberteam@Cybers-MacBook-Pro.local>
2022-11-05 23:00:43 +0200

    converting pwdtool to handler

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
-const { getAllUsersFromDB, getUserByIDFromDB } = require(".././handlers/users");
+const { getAllUsersFromDB, getUserByIDFromDB, isPasswordComplexed, isPasswordUsed } = require(".././handlers/users");
 const PWDTool = require("../utils/passwords");
+
 
 const getAllUsers = async (req, res = response) => {
     var queryResult = await getAllUsersFromDB();
@@ -15,19 +16,42 @@ const getUserByID = async (req, res = response) => {
 
 
 const changePassword = async (req, res = response) => {
-    var isAdmin = req.data.role == "Admin"
-
-    if(req.params.userID == req.data.userid || isAdmin){
-        isPasswordComplexed = PWDTool.isComplexed(req.body.newPassword)
-        // isPasswordUsed = PWDTool.isPasswordUsed(req.body.newPassword)
-        
-        
-        
-        console.log("Authorized")
-        res.status(200).json("OK")
+    try{
+        console.log('validating permissions')
+        var isAdmin = req.data.role == "Admin"
+        if(req.params.userID == req.data.userid || isAdmin){
+            console.log('validating complexity')
+            var isPasswordComplexed = isPasswordComplexed(req.body.newPassword)
+            
+            console.log('validating history')
+            var isPasswordUsed = isPasswordUsed(req.body.newPassword)
+            
+            console.log('validating password')
+            
+            if(validatePassword(req.body.username,req.body.oldPassword) == false){
+                res.status(500).send("Incorrect Password");
+            }
+            
+            if(isPasswordComplexed == false){
+                console.log("Password does not meet complexity requierments")
+                res.status(500).send("Password does not meet complexity requierments");
+            }
+            
+            if(isPasswordUsed == true){
+                console.log("Password does not meet password history requierments")
+                res.status(500).send("Password does not meet password history requierments");
+            }
+            
+            console.log('changing password')
+            await changePassword(req.body.username,req.body.newPassword)
+            res.status(200).json("OK")
+        }
+        else{
+            res.status(401).json("Unauthorized")
+        }
     }
-    else{
-        res.status(401).json("Unauthorized")
+    catch(error){
+        res.status(500).send(error)
     }
     
 };
