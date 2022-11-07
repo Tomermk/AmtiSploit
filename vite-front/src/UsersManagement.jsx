41da03900239551c736d47a0c5d02229b42d1c2e
tomermk <tomermk@gmail.com>
2022-11-08 01:23:14 +0200

    users mgmt page with useSWR

--- vite-front/src/UsersManagement.jsx
+++ vite-front/src/UsersManagement.jsx
@@ -1,11 +1,23 @@
 import {useContext, useEffect, useState} from 'react'
-import { Space, Table} from 'antd';
+import { Space, Table, Select, Button, Typography} from 'antd';
+import useSWR from 'swr';
+import useAxios from './utils/useAxios';
 import {AuthContext} from './context/AuthContext';
 import "./UsersManagement.css"
 
+
 export default function UsersManagement() {
+    const axiosAuth = useAxios();
+    const fetcher = url => axiosAuth.get(url).then(res => res.data)
+    const { data: users, loading, error} = useSWR("/users",fetcher);
     const {user, setCurrent} = useContext(AuthContext)
     const [isAdmin, setIsAdmin] = useState(false);
+    const [changedUsers, setChangedUsers] = useState({});
+    const [ userRoles, setUserRoles] = useState(users);
+    const roles = ['Admin', 'User'];
+    const {Option} = Select;
+    const { Title } = Typography;
+
 
     useEffect(() => {
         if(user.role !== 'Admin'){
@@ -16,20 +28,39 @@ export default function UsersManagement() {
         }
     }, [])
 
-    const data = [
-        {
-          username: 'Admin',
-          role: 'Admin',
-        },
-        {
-            username: 'tomermk@gmail.com',
-            role: 'User',
-        },
-        {
-            username: 'Amitay Biton',
-            role: 'Admin',
-        },
-      ];
+
+
+    function showDefaultRole(record) {
+        const defaultRole = userRoles.filter((user) => user.username === record.username).map((user) => user.userRole);
+        return defaultRole[0];
+    }
+
+    function handleSelectChange(value, record) {
+        const newUsersRoles = userRoles.map((user) => {
+            if(user.username === record.username){
+                user.userRole = value;
+            }
+            return user;
+        })
+        if(changedUsers[record.username]){
+            const {[record.username]: value, ...rest} = changedUsers;
+            setChangedUsers({...rest});
+        } else {
+            setChangedUsers({...changedUsers, [record.username]: value});
+        }
+        setUserRoles(newUsersRoles);
+    }
+
+    function renderHeader() {
+        return (
+            <div className="users-management-header">
+                <Space>
+                    <Button type="primary" onClick={() => console.log("update")} loading={loading}>Update</Button>
+                    <Button type="primary" onClick={() => console.log("add user")}>Add User</Button>
+                </Space>
+            </div>
+        )
+    }
 
     const columns = [
         {
@@ -41,21 +72,70 @@ export default function UsersManagement() {
             title: 'Role',
             dataIndex: 'role',
             key: 'role',
+            render: (text, record) => (
+                <Select
+                    defaultValue= {showDefaultRole(record)}
+                    value= {showDefaultRole(record)}
+                    onChange={(value) => handleSelectChange(value, record)}
+                    style={{ width: 120 }}
+                >
+                {roles.map((roleName) => {
+                return (
+                    <Option key={roleName} value={roleName}>
+                        {roleName}
+                    </Option>
+                )})}
+            </Select>
+            ),
+        },
+        {
+            title: 'First Name',
+            dataIndex: 'firstName',
+            key: 'firstName',
+        },
+        {
+            title: 'Last Name',
+            dataIndex: 'lastName',
+            key: 'lastName',
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
         },
         {
             title: 'Action',
+            dataIndex: 'action',
             key: 'action',
             render: (text, record) => (
-                <Space size="middle" key={text}>
-                    <a>Invite {record.username}</a>
-                    <a>Delete</a>
+                <Space size="middle" key={record.username}>
+                    <Button>Reset Password</Button>
+                    <Button danger={true}>Delete</Button>
                 </Space>
             ),
         },
     ];
-  return (
-    <div className="container">
-        { isAdmin && <Table columns={columns} dataSource={data} title={() => "Users Table"} footer={() => ""}/>}
-    </div>
-  )
+
+    if(error) {
+        return <div>failed to load</div>
+    }
+    if(users){
+        
+        const realData = users.map((user) => { 
+            return {
+                username: user.username,
+                role: user.userRole,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                createdAt: new Date(user.createdAt).toLocaleString('en-GB'),
+            }
+        })
+
+        return (
+            <div className="container">
+                <Title className="form-title">Users Management</Title>
+                { isAdmin && <Table columns={columns} dataSource={realData} loading={loading} title={renderHeader} footer={() => ""}/>}
+            </div>
+        )
+    }
 }
